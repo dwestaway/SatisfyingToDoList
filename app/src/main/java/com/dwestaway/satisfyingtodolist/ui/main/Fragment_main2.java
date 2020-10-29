77dd8e53e2b35f28cf51a70b922f46a1300e6858
dwestaway <danwesty1@googlemail.com>
2020-10-29 17:07:48 +0000

    First push
    Created tab layout, set colour scheme colour values

---
+++ app/src/main/java/com/dwestaway/satisfyingtodolist/ui/main/Fragment_main2.java
@@ -0,0 +1,23 @@
+package com.dwestaway.satisfyingtodolist.ui.main;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import com.dwestaway.satisfyingtodolist.R;
+
+/**
+ * A placeholder fragment containing a simple view.
+ */
+public class Fragment_main2 extends Fragment {
+
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_main, container, false);
+    }
+}
